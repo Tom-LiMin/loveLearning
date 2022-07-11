d2f46ac83b7af126a9c02c45ca66703f6a214d60
子木敏 <test@lm>
2022-07-11 18:46:37 +0800

    1集成mybatis生成器，设置生成器的覆盖模式， 2问题：自定义的sql会被覆盖，难顶 3简单使用Example

--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,6 +1,7 @@
 package com.course.server.service;
 
 import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,11 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> getList() {
-        return testMapper.getTestList();
+        TestExample testExample = new TestExample();
+        // 相当于一个where条件
+        testExample.createCriteria().andIdEqualTo("1");
+        testExample.setOrderByClause("id asc");
+        return testMapper.selectByExample(testExample);
     }
 
 }
