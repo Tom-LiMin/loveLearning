0d0b6f2aaa48f577c0540b5547c8f7e1289eaa33
子木敏 <test@lm>
2022-07-14 07:29:42 +0800

    初略了解前端项目的使用，同时删除不必要的前端页面元素， 先初级修改下标题，

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="main-container">
-    <div class="main-content">
       <div class="row">
         <div class="col-sm-10 col-sm-offset-1">
           <div class="login-container">
@@ -27,28 +26,30 @@
                     <form>
                       <fieldset>
                         <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username" />
-															<i class="ace-icon fa fa-user"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input type="text" class="form-control" placeholder="Username"/>
+                            <i class="ace-icon fa fa-user"></i>
+                          </span>
                         </label>
 
                         <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password" />
-															<i class="ace-icon fa fa-lock"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input type="password" class="form-control" placeholder="Password"/>
+                            <i class="ace-icon fa fa-lock"></i>
+                          </span>
                         </label>
 
                         <div class="space"></div>
 
                         <div class="clearfix">
                           <label class="inline">
-                            <input type="checkbox" class="ace" />
+                            <input type="checkbox" class="ace"/>
                             <span class="lbl">记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-primary"
+                                  @click="login()">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
@@ -67,13 +68,21 @@
           </div>
         </div><!-- /.col -->
       </div><!-- /.row -->
-    </div><!-- /.main-content -->
   </div><!-- /.main-container -->
 </template>
 
 <script>
-$('body').attr('class', 'login-layout light-login');
 export default {
-  name: 'app',
+  name: "login",
+  mounted: function() {
+    $("body").removeClass("no-skin");
+    $("body").attr("class", "login-layout light-login");
+
+  },
+  methods: {
+    login () {
+      this.$router.push("/admin")
+    }
+  }
 }
 </script>
