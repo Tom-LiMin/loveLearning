9a3fccea35f470cdc67013b4ce1b72cbba3201fe
子木敏 <test@lm>
2022-08-07 20:01:41 +0800

    1.课程列表中显示讲师信息 2.课程名称字体自适应

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -35,6 +35,20 @@
             <h3 class="search-title">
               <a href="#" class="blue">{{course.name}}</a>
             </h3>
+
+            <div v-for="teacher in teachers.filter(t=>{return t.id===course.teacherId})" class="profile-activity clearfix">
+              <div>
+                <img v-show="!teacher.image" class="pull-left" src="/ace/assets/images/avatars/avatar5.png">
+                <img v-show="teacher.image" class="pull-left" v-bind:src="teacher.image">
+                <a class="user" href="#"> {{teacher.name}} </a>
+                <br>
+                {{teacher.position}}
+              </div>
+            </div>
+
+            <p>
+              <span class="blue bolder bigger-150"><i class="fa fa-rmb"></i>&nbsp;{{course.price}}</span>&nbsp;
+            </p>
             <p>{{course.summary}}</p>
             <p>
               <span class="badge badge-info">{{course.id}}</span>
@@ -555,7 +569,13 @@ export default {
 </script>
 
 <style scoped>
-.caption h3 {
-  font-size: 20px;
-}
+  .caption h3 {
+    font-size: 20px;
+  }
+
+  @media (max-width: 1199px) {
+    .caption h3 {
+      font-size: 16px;
+    }
+  }
 </style>
