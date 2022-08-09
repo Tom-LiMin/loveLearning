5badcbb37770270c4b091a45db9efe4a00539c45
子木敏 <test@lm>
2022-08-09 18:47:02 +0800

    1.文件上传功能开发；制作文件上传公共组件 2.修复不能连续上传同一个文件的BUG

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -83,36 +83,35 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                  <button type="button" v-on:click="selectImage()" class="btn btn-white btn-default btn-round">
-                    <i class="ace-icon fa fa-upload"></i>
-                    上传头像
-                  </button>
-                  <input class="hidden" ref="file" type="file" v-on:change="uploadImage()" id="file-upload-input">
+                  <file v-bind:id="'image-upload'"
+                        v-bind:text="'上传头像'"
+                        v-bind:suffixs="['jpg','jpeg','png']"
+                        v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
                     </div>
                   </div>
                 </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-2 control-label">职位</label>
+              <div class="col-sm-10">
+                <input v-model="teacher.position" class="form-control">
               </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">职位</label>
-                <div class="col-sm-10">
-                  <input v-model="teacher.position" class="form-control">
-                </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-2 control-label">座右铭</label>
+              <div class="col-sm-10">
+                <input v-model="teacher.motto" class="form-control">
               </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">座右铭</label>
-                <div class="col-sm-10">
-                  <input v-model="teacher.motto" class="form-control">
-                </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-2 control-label">简介</label>
+              <div class="col-sm-10">
+                <textarea input v-model="teacher.intro" class="form-control" rows="5"></textarea>
               </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">简介</label>
-                <div class="col-sm-10">
-                  <textarea input v-model="teacher.intro" class="form-control" rows="5"></textarea>
-                </div>
-              </div>
+            </div>
             </form>
           </div>
           <div class="modal-footer">
@@ -127,8 +126,10 @@
 
 <script>
   import Pagination from "../../components/pagination";
+  import File from "../../components/file";
+  // import File from "@/components/file";
   export default {
-    components: {Pagination},
+    components: {File, Pagination},
     name: "business-teacher",
     data: function() {
       return {
@@ -229,42 +230,10 @@
         });
       },
 
-      uploadImage() {
+      afterUpload (resp) {
         let _this = this;
-        let formData = new window.FormData();
-        let file = _this.$refs.file.files[0];
-
-        // 判断文件格式
-        let suffixs = ["jpg","jpeg","png"];
-        let fileName = file.name;
-        let suffix = fileName.substring(fileName.lastIndexOf(".")+1,fileName.length).toLowerCase();
-        let validateSuffix = false;
-        for (let i = 0,l = suffixs.length; i < l ; i++) {
-          if (suffixs[i] === suffix ) {
-            validateSuffix = true;
-            break;
-          }
-        }
-
-        if (!validateSuffix) {
-          Toast.warning("文件格式不正确！只支持上传："+suffixs.join(","));
-          return;
-        }
-
-        // key："file"必须和后端controller参数名一致
-        formData.append('file', file);
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          let image = resp.content;
-          console.log("头像地址",image);
-          _this.teacher.image = image;
-        });
-      },
-
-      selectImage() {
-        $("#file-upload-input").trigger("click");
+        let image = resp.content;
+        _this.teacher.image = image;
       }
     }
   }
